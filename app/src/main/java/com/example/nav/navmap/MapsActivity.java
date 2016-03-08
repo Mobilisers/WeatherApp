5a345e0878cfd38b51e65f0ff00a6b5524f2bacc
Navaneet Sarma <navaneet.sarma@diona.com>
2016-03-08 14:00:08 +0530

    location check added

--- app/src/main/java/com/example/nav/navmap/MapsActivity.java
+++ app/src/main/java/com/example/nav/navmap/MapsActivity.java
@@ -2,14 +2,18 @@ package com.example.nav.navmap;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.provider.Settings;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
@@ -209,5 +213,27 @@ public class MapsActivity extends FragmentActivity implements LocationListener {
     public void onProviderDisabled(String provider) {
 
         Log.e(getLocalClassName(),"location provider is disabled");
+
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setMessage(getApplicationContext().getResources().getString(R.string.gps_network_not_enabled));
+        dialog.setPositiveButton(getApplicationContext().getResources().getString(R.string.open_location_settings), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                // TODO Auto-generated method stub
+                Intent myIntent = new Intent( Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                myIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                getApplicationContext().startActivity(myIntent);
+                //get gps
+            }
+        });
+        dialog.setNegativeButton(getApplicationContext().getString(R.string.Cancel), new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                // TODO Auto-generated method stub
+
+            }
+        });
+        dialog.show();
     }
 }
