cfe41f7d04ac91680e7d36d3f5dc1a7089e3ed11
Navaneet Sarma <navaneet.sarma@diona.com>
2016-03-10 20:35:43 +0530

    fix

--- app/src/main/java/com/example/nav/navmap/MapsActivity.java
+++ app/src/main/java/com/example/nav/navmap/MapsActivity.java
@@ -1,6 +1,8 @@
 package com.example.nav.navmap;
 
 import android.graphics.Bitmap;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -12,6 +14,7 @@ import android.widget.Toast;
 
 import com.example.nav.navmap.models.Main;
 import com.example.nav.navmap.models.Root;
+import com.example.nav.navmap.models.Sys;
 import com.example.nav.navmap.models.Weather;
 import com.example.nav.navmap.models.Wind;
 import com.google.android.gms.maps.CameraUpdate;
@@ -25,6 +28,10 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import org.json.JSONException;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity {
 
@@ -146,12 +153,40 @@ public class MapsActivity extends FragmentActivity {
                 Weather weather = root.getWeather()[0];
                 Main main = root.getMain();
                 Wind wind = root.getWind();
+                Sys sys = root.getSys();
                 condDescr.setText(weather.getMain() + " (" + weather.getDescription() + ")");
                 temp.setText("" + Math.round((Double.valueOf(main.getTemp()) - 273.15)) + DEGREE + "C");
                 hum.setText("" + main.getHumidity() + "%");
                 press.setText("" + main.getPressure() + " hPa");
                 windSpeed.setText("" + wind.getSpeed() + " mps");
                 windDeg.setText("" + wind.getDeg() + DEGREE);
+                Geocoder geocoder;
+                geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+                List<Address> addresses = null;
+                try {
+                    addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if (addresses != null && addresses.size() > 0) {
+                    String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+                    String city = addresses.get(0).getLocality();
+                    String state = addresses.get(0).getAdminArea();
+                    String country = addresses.get(0).getCountryName();
+                    String postalCode = addresses.get(0).getPostalCode();
+                    String knownName = addresses.get(0).getFeatureName(); // Only if available else return NULL
+                    if (country != null) {
+                        if (state == null) {
+                            cityText.setText(country);
+                        } else {
+                            cityText.setText(country + " (" + state + ")");
+                        }
+                    } else {
+                        cityText.setVisibility(View.GONE);
+                    }
+                } else {
+                    cityText.setVisibility(View.GONE);
+                }
                 //imgView.setImageDrawable(getResources().getDrawable(R.drawable.wally));
                 Log.e(getLocalClassName(), imgView + " " + weather.getIcon());
                 imgView.setImageBitmap(new ImageUtil().StringToBitMap(weather.getIcon()));
